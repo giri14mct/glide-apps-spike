f289279cfa4a6140146ece736546a104c1117ace
Giritharan <70332966+giri14mct@users.noreply.github.com>
2024-03-17 22:24:00 +0530

    Feat: Added glide apps configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,156 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+import "@glideapps/glide-data-grid/dist/index.css";
+import { useExtraCells } from "@glideapps/glide-data-grid-cells";
+
+import {
+  DataEditor,
+  GridCell,
+  GridCellKind,
+  GridColumn,
+  EditableGridCell,
+  Item,
+  GridColumnIcon,
+} from "@glideapps/glide-data-grid";
+
+interface TableData {
+  id: number;
+  name: string;
+  completed: boolean;
+  color: string[];
+}
+
+export default function App() {
+  const [commentData, setCommentData] = useState<TableData[]>([]);
+  const cellProps: object = useExtraCells();
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://65f70b7db4f842e808850415.mockapi.io/api/v1/todos"
+      );
+      const json = await response.json();
+      setCommentData(json);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const appendRow = () => {
+    const newRow: TableData = {
+      id: commentData.length + 1,
+      name: "New Task",
+      completed: false,
+      color: ["red", "green", "yellow", "blue"],
+    };
+    setCommentData([...commentData, newRow]);
+  };
+
+  const columns: GridColumn[] = [
+    { title: "ID", width: 100, icon: GridColumnIcon.HeaderNumber },
+    { title: "Name", width: 800, icon: GridColumnIcon.HeaderString },
+    { title: "Completed", width: 100 },
+    { title: "Color", width: 100, icon: GridColumnIcon.HeaderMarkdown },
+  ];
+
+  const colorOptions = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Yellow", value: "yellow" },
+    { label: "Blue", value: "blue" },
+  ];
+
+  const getData = React.useCallback(
+    (cell: Item): GridCell => {
+      const [col, row] = cell;
+      const dataRow = commentData[row];
+      const indexes = ["id", "name", "completed", "color"] as const;
+      const index = indexes[col];
+      const d = String(dataRow[index]);
+
+      if (col === 0 || col === 1) {
+        return {
+          kind: GridCellKind.Text,
+          allowOverlay: true,
+          displayData: d,
+          readonly: false,
+          data: d,
+        };
+      } else if (col === 2) {
+        return {
+          kind: GridCellKind.Boolean,
+          readonly: false,
+          allowOverlay: false,
+          data: Boolean(d),
+        };
+      } else if (col === 3) {
+        return {
+          kind: GridCellKind.Custom,
+          allowOverlay: true,
+          readonly: false,
+          copyData: "4",
+          data: {
+            kind: "dropdown-cell",
+            allowedValues: d.split(","),
+            value: d.split(",")[row] || "red",
+          },
+        };
+      } else {
+        return {
+          kind: GridCellKind.Text,
+          allowOverlay: true,
+          displayData: d,
+          readonly: false,
+          data: d,
+        };
+      }
+    },
+    [commentData]
+  );
+
+  const onCellEdited = React.useCallback(
+    (cell: Item, newValue: EditableGridCell) => {
+      if (newValue.kind === GridCellKind.Text) {
+        console.log("changedTextValue", newValue.data);
+      } else if (newValue.kind === GridCellKind.Boolean) {
+        console.log(newValue, "changedBooleanValue");
+      } else {
+        return;
+      }
+    },
+    []
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <DataEditor
+        {...cellProps}
+        columns={columns}
+        getCellContent={getData}
+        rows={commentData.length}
+        onCellEdited={onCellEdited}
+      />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <button
+        onClick={appendRow}
+        style={{
+          backgroundColor: "#4CAF50",
+          color: "white",
+          padding: "10px 20px",
+          margin: "10px",
+          border: "none",
+          cursor: "pointer",
+          borderRadius: "5px",
+          marginTop: "2rem",
+        }}
+      >
+        Add New Row
+      </button>
+    </>
   );
 }
